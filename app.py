ee1c190a4a70bfa3e028a7a76b053d27ce6e2a1a
GailiusR <gailiusr@mail.ru>
2025-03-17 17:54:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,12 +7,10 @@ app = Flask(__name__, static_folder='.', template_folder='.')
 def index():
     return render_template('index.html')
 
-# Маршрут для раздачи любых статических файлов из корня проекта
 @app.route('/<path:filename>')
 def serve_static(filename):
     return send_from_directory('.', filename)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
-
+    app.run(host='0.0.0.0', port=port, debug=False)
